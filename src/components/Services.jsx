df2475bfdff9aa7f92ae0a9e5266dc1ec3e668bf
unknown <nechetov@hotmail.com>
2021-03-31 23:06:39 +0300

    add experience component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,7 +9,7 @@ const Services = () => {
       <h1 className="py-5">my services</h1>
       <div className="container">
         <div className="row">
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faDesktop} size="2x" />
@@ -19,7 +19,7 @@ const Services = () => {
             </div>
           </div>
           {/* - */}
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faFileCode} size="2x" />
@@ -30,7 +30,7 @@ const Services = () => {
             </div>
           </div>
           {/* - */}
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faFacebookF} size="2x" />
@@ -41,7 +41,7 @@ const Services = () => {
             </div>
           </div>
           {/* - */}
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faGoogle} size="2x" />
