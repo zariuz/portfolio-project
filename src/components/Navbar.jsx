4c2ea4eb5c2a912048e2ead572640db135510820
zariuz <nechetov@hotmail.com>
2021-04-02 10:06:46 +0300

    add portfolio component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from '../logo.png';
 // REACT FONTAWESOME IMPORTS
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
         <a className="navbar-brand" href="#">
-          <img className="logo" src={logo} alt="logo..." />
+          <span class="logo">ЕН</span>
         </a>
         <button
           className="navbar-toggler"
@@ -19,7 +19,7 @@ const Navbar = () => {
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation">
-          <FontAwesomeIcon icon={faBars} style={{ color: '#fff' }} />
+          <FontAwesomeIcon icon={faBars} style={{color: '#fff'}} />
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
